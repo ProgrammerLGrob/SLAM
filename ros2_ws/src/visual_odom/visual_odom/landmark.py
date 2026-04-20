fix: visual_odom - Rotation and Translation fixed - but ransac missing

ros2_ws/src/visual_odom/visual_odom/landmark.py
@@ -23,6 +23,7 @@ class landmark:
 """
 def kabash(P_i: np.ndarray, Q_i: np.ndarray):
 
+    
     print("P_i: ", P_i)
     print("Q_i: ", Q_i)
 
@@ -43,9 +44,8 @@ def kabash(P_i: np.ndarray, Q_i: np.ndarray):
     theta = atan2(first_sum, sec_sum)
     
     #Hier steckt noch ein Fehler drin da die rotation um z in world hier nicht passend ist 
-    R = np.array([[ np.cos(theta), -np.sin(theta), 0],
-                  [ np.sin(theta),  np.cos(theta), 0],
-                  [ 0, 0, 1]])
+    R = np.array([[ np.cos(theta), -np.sin(theta)],
+                  [ np.sin(theta),  np.cos(theta)]])
     
     t = m_P - R @ m_Q
 

ros2_ws/src/visual_odom/visual_odom/visual_odom_node.py
@@ -103,11 +103,14 @@ class VisualOdom(Node):
 
 
                 if matches[m].distance < DESCRIPTOR_TOLERANCE and z_first > MIN_DEPTH and z_first < MAX_DEPTH and z_end > MIN_DEPTH and z_end < MAX_DEPTH:
-                    P_i.append(self.calculate_coordinate(u_first, v_first, z_first))
-                    Q_i.append(self.calculate_coordinate(u_end, v_end, z_end))
+                    coor = self.calculate_coordinate(u_first, v_first, z_first)
+                    P_i.append([coor[0], coor[2]])
+                    coor = self.calculate_coordinate(u_end, v_end, z_end)
+                    Q_i.append([coor[0], coor[2]])
                     valid_count += 1
 
             if len(P_i) >= 2:
+
                 kabash_result = kabash(np.array(P_i), np.array(Q_i))
                 self.get_logger().info(f"Rotation: {kabash_result[0]}, Translation: {kabash_result[1]}, Theta: {kabash_result[2]*180/pi} degrees")
             else:
@@ -119,11 +122,10 @@ class VisualOdom(Node):
 
 
     def calculate_coordinate(self, u, v, z):
-        z = z
         y = z * (v-CV)/F
         x = z * (u-CU)/F
 
-        p3d_vector = np.array([x, y, z])/1000.0
+        p3d_vector = np.array([x, y, z])
         return p3d_vector
 
 